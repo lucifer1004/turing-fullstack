a10a138e377289953bcf8b1c6ecde46083d70d63
Gabriel Wu <qqbbnease1004@126.com>
2019-03-09 15:05:24 +0800

    checkpoint: basic nest module

---
+++ api/src/users/users.controller.spec.ts
@@ -0,0 +1,22 @@
+import {Test, TestingModule} from '@nestjs/testing'
+import {UsersController} from './users.controller'
+import {UsersService} from './users.service'
+
+describe('Users Controller', () => {
+  let controller: UsersController
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService],
+    }).compile()
+
+    controller = module.get<UsersController>(UsersController)
+  })
+
+  describe('/users', () => {
+    it('should return all users', () => {
+      expect(controller.getUsers()).toEqual(['Tom', 'Mary'])
+    })
+  })
+})
